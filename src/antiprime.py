706c3231c74c81c08bd4bda8441ba8526b4a4837
Daniela Gonzalez <daniela.gonzalez03@estudiant.upf.edu>
2024-10-21 18:22:14 +0200

    First attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,10 +1,30 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+def main():
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
-
+	i = 1
+	c = 0
+	while i <= x:
+		if x % i == 0:
+			c = c + 1
+		i = i + 1
+	l = x - 1
+	s = 0
+	while l >= 1 and s < c:
+		a = 1
+		s = 0
+		while a <= l:
+			if l % a == 0:
+				s = s + 1
+			a = a + 1
+		l = l - 1
+	if s >= c:
+		res = "not anti-prime"
+		return("not anti-prime")
+	else:
+		res = "anti-prime"
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
@@ -12,9 +32,13 @@ def main() :
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
 	return("anti-prime")
+	
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+	import sys
+	if len(sys.argv) > 1:
+		x = int(sys.argv[1]) 
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
